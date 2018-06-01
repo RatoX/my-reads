b252aba9e3f9df8808bee99c9bca141f3ff8abcf
RatoX <rodrigovieira18@gmail.com>
2018-06-01 11:30:24 -0300

    Add SearchButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MyShelf from './components/MyShelf';
+import SearchButton from './components/SearchButton';
 
 class App extends Component {
   render () {
     return (
       <main>
         <MyShelf></MyShelf>
+        <SearchButton></SearchButton>
       </main>
     );
   }
