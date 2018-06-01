fd67a2c2d6d7794da14135e29194fc18da896bba
RatoX <rodrigovieira18@gmail.com>
2018-06-01 14:55:23 -0300

    Refactor Shelf

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,32 +1,40 @@
 import './Shelf.css';
-import React, { Component } from 'react';
+import React from 'react';
 import Book from './Book';
 import StatusIcon from './StatusIcon';
+import PropTypes from "prop-types";
 
-class Shelf extends Component {
+function Shelf ({ title, books, statusIcon }) {
+  const statusIconClassName = `shelf__icon shelf__status-${statusIcon}`
 
-  render() {
-    const { title, books, statusIcon } = this.props
-    const statusIconClassName = `shelf__icon shelf__status-${statusIcon}`
-
-    return (
-      <section className="shelf">
-        <header className="shelf__title">
-          <StatusIcon className={statusIconClassName} status={statusIcon}></StatusIcon>
-          { title }
-        </header>
-        <ol className="shelf__books">
-          { books.map((book) =>
-            <li className="shelf__book" key={book.id}>
-              <Book id={book.id} title={book.title} imageLinks={book.imageLinks} authors={book.authors}></Book>
-            </li>
-            )
-          }
-        </ol>
-      </section>
-    );
-  }
-
+  return (
+    <section className="shelf">
+      <header className="shelf__title">
+        <StatusIcon className={statusIconClassName} status={statusIcon}></StatusIcon>
+        { title }
+      </header>
+      <ol className="shelf__books">
+        { books.map((book) =>
+          <li className="shelf__book" key={book.id}>
+            <Book id={book.id} title={book.title} imageLinks={book.imageLinks} authors={book.authors}></Book>
+          </li>
+          )
+        }
+      </ol>
+    </section>
+  );
 }
+
+Shelf.propTypes = {
+  title: PropTypes.string,
+  books: PropTypes.array,
+  statusIcon: PropTypes.string,
+};
+
+Shelf.defaultProps = {
+  title: '',
+  books: [],
+  statusIcon: '',
+};
 
 export default Shelf;
