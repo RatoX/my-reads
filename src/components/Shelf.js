097abdf3bd8a6a61e88d5fed51431a995ffa04b6
RatoX <rodrigovieira18@gmail.com>
2018-06-02 10:39:38 -0300

    Add modal for loading screen

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,59 +1,73 @@
 import './Shelf.css';
-import React from 'react';
+import React, { Component } from 'react'
 import Book from './Book';
 import ShelfIcon from './ShelfIcon';
 import PropTypes from "prop-types";
 
-function Shelf ({ title, books, shelfIcon, onChangeBookShelf }) {
-  const statusIconClassName = `shelf__icon shelf__status-${shelfIcon}`
-  const onBeforeUpdate = () => {
-    console.log('TROCANDO')
-  }
-  const onAfterUpdate = (book, newShelf) => {
-    onChangeBookShelf(book, newShelf)
+class Shelf extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    books: PropTypes.array,
+    shelfIcon: PropTypes.string,
+    onChangeBookShelf: PropTypes.func,
+  };
+
+  static defaultProps = {
+    title: '',
+    books: [],
+    shelfIcon: '',
+    onChangeBookShelf: () => {},
+  };
+
+  state = {
+    loading: false,
   }
 
-  return (
-    <section className="shelf">
-      <header className="shelf__title">
-        <ShelfIcon
-          className={statusIconClassName}
-          shelf={shelfIcon}>
-        </ShelfIcon>
-        { title }
-      </header>
-      <ol className="shelf__books">
-        { books.map((book) =>
-          <li className="shelf__book" key={book.id}>
-            <Book
-              id={book.id}
-              title={book.title}
-              imageLinks={book.imageLinks}
-              shelf={book.shelf}
-              authors={book.authors}
-              onBeforeUpdate={onBeforeUpdate}
-              onAfterUpdate={onAfterUpdate.bind(this, book)}>
-            </Book>
-          </li>
-          )
-        }
-      </ol>
-    </section>
-  );
+  render () {
+    const { title, books, shelfIcon, onChangeBookShelf } = this.props
+    const { loading } = this.state
+    const statusIconClassName = `shelf__icon shelf__status-${shelfIcon}`
+    const onBeforeUpdate = () => {
+      this.setState({ loading: true })
+    }
+    const onAfterUpdate = (book, newShelf) => {
+      onChangeBookShelf(book, newShelf)
+      this.setState({ loading: false })
+    }
+
+    let shelfClassName = `shelf`
+    if (loading) {
+     shelfClassName += ` ${shelfClassName}_loading`
+    }
+
+    return (
+      <section className={shelfClassName}>
+        <header className="shelf__title">
+          <ShelfIcon
+            className={statusIconClassName}
+            shelf={shelfIcon}>
+          </ShelfIcon>
+          { title }
+        </header>
+        <ol className="shelf__books">
+          { books.map((book) =>
+            <li className="shelf__book" key={book.id}>
+              <Book
+                id={book.id}
+                title={book.title}
+                imageLinks={book.imageLinks}
+                shelf={book.shelf}
+                authors={book.authors}
+                onBeforeUpdate={onBeforeUpdate}
+                onAfterUpdate={onAfterUpdate.bind(this, book)}>
+              </Book>
+            </li>
+            )
+          }
+        </ol>
+      </section>
+    );
+  }
 }
-
-Shelf.propTypes = {
-  title: PropTypes.string,
-  books: PropTypes.array,
-  shelfIcon: PropTypes.string,
-  onChangeBookShelf: PropTypes.func,
-};
-
-Shelf.defaultProps = {
-  title: '',
-  books: [],
-  shelfIcon: '',
-  onChangeBookShelf: () => {},
-};
 
 export default Shelf;
